8346aa01f550c27f3d6a3d53eece943771f29836
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-11-09 13:37:41 -0500

    playing around with the contact info

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Mailto from 'react-protected-mailto'
 
 export default class Contact extends React.Component{
   render(){
     return (
       <div>
-        <h1>This is the Contact page!</h1>
+        <p>
+          Phone: <Mailto tel="917-345-6194" />
+          <br />
+          Email:{" "}
+          <Mailto
+            email="ayarza.manwaring@gmail.com" obfuscate={true}
+            headers={
+              { subject: "Question from the website" }
+            }
+          >Contact Me! </Mailto>
+        </p>
       </div>
-    )
+    );
   }
 }
