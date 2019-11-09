fee42a28e09bd7b6935436aaa361de6b3583d58a
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-11-09 13:51:10 -0500

    playing around with navbar css and attempting to obfuscate email address

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Mailto from 'react-protected-mailto'
 
+
 export default class Contact extends React.Component{
   render(){
     return (
       <div>
-        <p>
+        <p> 
           Phone: <Mailto tel="917-345-6194" />
           <br />
           Email:{" "}
           <Mailto
-            email="ayarza.manwaring@gmail.com" obfuscate={true}
+            email="ayarza.manwaring@gmail.com"
             headers={
-              { subject: "Question from the website" }
+              ({ subject: "Question from the website" })
             }
-          >Contact Me! </Mailto>
+          />
         </p>
       </div>
     );
