5ada8aab276462ab0fa46bde3a3020914219a3c1
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-01-22 16:57:34 -0500

    added bootstrap to package JSON and jquery added back the navbar using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
 import React from 'react';
 
-import{BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import{BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import './App.css';
+
 
 class App extends React.Component{
   render(){
     return(
       <Router>
         <div className='content'>
-          {/* <nav className='navBar'> */}
-            {/* <Link to="/">Home</Link> */}
-            {/* <Link to="/about">About</Link> */}
-            {/* <Link to="/projects">Portfolio</Link> */}
-            {/* <Link to="/contact">Contact Me</Link> */}
-            {/* <p>  */}
-          {/* Phone: <Mailto tel="917-345-6194" /> */}
-          {/* <br /> */}
-          {/* Email:{" "} */}
-          {/* <Mailto
+            <Navbar expand='lg' bg="dark" variant="dark">
+              <Navbar.Brand href="#home">
+                 {/* <img
+                  alt=""
+                  src="/logo.svg"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{' '} */}
+                React Bootstrap
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#about">About</Nav.Link>
+                  <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                  <Nav.Link href="#contact">Contact</Nav.Link>
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>  */}
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+
+          {/* <nav className='navBar'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Portfolio</Link>
+            <Link to="/contact">Contact Me</Link>
+            <p> 
+          Phone: <Mailto tel="917-345-6194" />
+          <br />
+          Email:{" "}
+          {<Mailto
             email="***REMOVED***"
             headers={
               ({ subject: "Question from the website" })
             }
-          /> */}
-        {/* </p> */}
-          {/* </nav> */}
+          />}
+        </p>
+          </nav> */}
           <main>
             {/* <h1> This is the Home Page</h1> */}
           </main>
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import {Container, Col, Row } from 'react-bootstrap'
 
 export default class Projects extends React.Component{
   render(){
     return (
-      <div className='content'>
-        <h1>This is the Projects page!</h1>
+      <Container>
+        <div className='projects'>
+          <Row className='projectsRow'>
+            <Col className ='projectsCol'>
+            <div className='individual'>
+          <img className='projectsImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt='Project One'/>
+          <div className='description'>
+           <h3>Island Shopper</h3>
+          <p>Description</p>
+          <a href='https://yachtshopper.herokuapp.com/'>Deployed Site</a>
+          <br></br>
+          <a href='https://github.com/yachtworld/yachtshopper'>Github Repository</a> 
+          </div>  
+        </div>
+        <br></br></Col>
+          <Col className ='projectsCol'>
+          
+        <div className='individual'>
+          <img className='projectsImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt='Project Two'/>
+          <div className='description'>
+            <h3>Hike With Me</h3>
+          <p>Description</p>
+          <a href='https://github.com/ayarzam/HikeWithMe'>Github Repository</a>
+          </div>
+          </div>
+          <br></br>
+          </Col>
+        </Row>
+        <Row className='projectsRow'>
+          <Col  className ='projectsCol'>
+            <div className='individual'>
+          <img className='projectsImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt='Project Three'/>
+          <div className='description'>
+            <h3>Music Mapper</h3>
+          <p>Description</p>
+          <a href='https://spotify-music-mapper.herokuapp.com/'>Deployed Site</a>
+          <br></br>
+          <a href='https://github.com/music-mapper/music-mapper'>Github Repository</a>
+          </div>
+          </div>
+          <br></br>
+          </Col>
+          <Col className ='projectsCol'> 
+        
+          <div className='individual'>
+          <img className='projectsImg' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt='Project Four'/>
+          <div className='description'>
+            <h3>Nodejs Card Game</h3>
+          <p>Description</p>
+          <a href='https://github.com/ayarzam/card_game'>Github Repository</a>
+          </div>
+          </div>
+          </Col>
+          </Row>
+        
       </div>
+      </Container>
+      
     )
   }
 }
