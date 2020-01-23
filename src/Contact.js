8b4ddfe0fb7c64fb51ab588adda23ae9169c44e7
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-01-22 22:47:16 -0500

    Revert "Updates"
    This reverts commit faa013f1e02f3a95d75f69f7d9fae3f0671904d3.

---
+++ src/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Mailto from 'react-protected-mailto'
+
+export default class Contact extends React.Component{
+  render(){
+    return (
+      <div className='content'>
+        <p> 
+          Phone: <Mailto tel="917-345-6194" />
+          <br />
+          Email:{" "}
+          <Mailto
+            email="***REMOVED***"
+            headers={
+              ({ subject: "Question from the website" })
+            }
+          />
+        </p>
+      </div>
+    );
+  }
+}
