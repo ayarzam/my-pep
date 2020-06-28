0d21d418d4b501b9788490aff2bc0838a735d8b5
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-06-27 18:05:33 -0700

    The original cloned project had multiple vulnerabilities and didn't fully employ the capabilities of react-bootstrap. To fix this I created a new react-app and copied over the project files. I then created a sass file and used a live sass compiler to style the website. I also eliminated unecessary css and ensured react-bootstrap was being used.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './css/App.scss';
 
-import{BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import { Navbar, Nav, Dropdown, SplitButton } from 'react-bootstrap'
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './views/Home';
+import About from './views/About';
+import Projects from './views/Projects';
+import Contact from './views/Contact';
+import { Navbar, Nav, Dropdown, ButtonGroup } from 'react-bootstrap';
 
-
-class App extends React.Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <Router>
-        <div className='content'>
-          <Navbar id="navbar" expand={'md' || 'lg' || 'xl'} bg="dark" variant="dark">
-            <div id="navbar-contents">
-              <Link to="/">
-                <Navbar.Brand>
-                  <img className='logo d-inline-block align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt=""/>{' '}
-                  React Bootstrap
-                </Navbar.Brand>
-              </Link>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              </div>
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Link to='/about'>About</Link>
-                  {/* <Nav.Link href="/about">About</Nav.Link> */}
-                  
-                  <div className="dropdown">
-                    <Link to='/projects'>
-                    <SplitButton
-                          key='down'
-                          id='dropdown-button-drop-down'
-                          drop='down'
-                          variant="secondary"
-                          title='Portfolio'
-                          href="/projects"
-                        >
-                          <Dropdown.Item eventKey="1">Project1</Dropdown.Item>
-                          <Dropdown.Item eventKey="2">Project2</Dropdown.Item>
-                          <Dropdown.Item eventKey="3">Project3</Dropdown.Item>
-                          <Dropdown.Divider className="nav-divider"/>
-                          <Dropdown.Item eventKey="4">Github</Dropdown.Item>
-                        </SplitButton>{' '}
-                    </Link>
-                        
-                  </div>
-                  <Link to='/contact'>Contact</Link>
-                  {/* <Nav.Link href="/contact">Contact</Nav.Link> */}
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          
-          <main>
-            {/* <h1> This is the Home Page</h1> */}
-          </main>
-             <Switch>
-            <Route exact path="/" render={routeProps => <Home {...routeProps} />} />
-            <Route exact path="/about" render={routeProps => <About {...routeProps} />}/>
-            <Route exact path="/projects" render={routeProps => <Projects {...routeProps} />} />
-            <Route exact path="/contact" render={routeProps => <Contact {...routeProps} />} />
-            </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/projects" component={Projects}></Route>
+          <Route path="/contact" component={Contact}></Route>
+        </Switch>
+
+        <header>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+            <Navbar.Brand href="/">
+              <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
+              React-Bootstrap
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link className="m-auto" href="/about">About</Nav.Link>
+                <Dropdown className="m-auto" as={ButtonGroup}>
+                  <Nav.Link href="/projects">Projects</Nav.Link>
+
+                  <Dropdown.Toggle split variant="transparent" id="dropdown-split-btn" />
+                  <Dropdown.Menu id="dropdown-menu">
+                    <Dropdown.Item eventKey="1">Project1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Project2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">Project3</Dropdown.Item>
+                    <Dropdown.Divider className="nav-divider" />
+                    <Dropdown.Item eventKey="4">Github</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+
+                <Nav.Link className="m-auto" href="/contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
       </Router>
-    )
+    );
   }
 }
 
