4c5e757a8926ec12aa7678da4439c72460d8ae02
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-06-29 15:55:55 -0700

    Testing out the smooth scroll.

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,15 +12,40 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className='content'>
-        <div>
-          <img alt="Me" src='https://media.licdn.com/dms/image/C5603AQGWlDKs0xonxg/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=fjmXGnmmXpCWi7Dgd0nU_BHG0yFXDJUqXf_8Nm-zK4c' />
-        </div>
-        <div className='content'>
-          <h3>Hello! </h3>
-          <h3>Welcome to my portfolio. My name is Ayarza Manwaring and I recently finished a CS coding bootcamp.</h3>
-        </div>
-        <Link to="/projects"><h1>Check out my projects</h1></Link>
+      <div>
+        <section>
+          <div>
+            <img alt="Me" src='https://media.licdn.com/dms/image/C5603AQGWlDKs0xonxg/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=fjmXGnmmXpCWi7Dgd0nU_BHG0yFXDJUqXf_8Nm-zK4c' />
+          </div>
+          <div className='content'>
+            <h3>Hello! </h3>
+            <h3>Welcome to my portfolio. My name is Ayarza Manwaring and I recently finished a CS coding bootcamp.</h3>
+          </div>
+          <Link to="/projects"><h1>Check out my projects</h1></Link>
+          
+        </section>
+
+        <section>
+          <div>
+            <img alt="Me" src='https://media.licdn.com/dms/image/C5603AQGWlDKs0xonxg/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=fjmXGnmmXpCWi7Dgd0nU_BHG0yFXDJUqXf_8Nm-zK4c' />
+          </div>
+          <div className='content'>
+            <h3>Hello! </h3>
+            <h3>Welcome to my portfolio. My name is Ayarza Manwaring and I recently finished a CS coding bootcamp.</h3>
+          </div>
+          <Link to="/projects"><h1>Check out my projects</h1></Link>
+        </section>
+
+        <section>
+          <div>
+            <img alt="Me" src='https://media.licdn.com/dms/image/C5603AQGWlDKs0xonxg/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=fjmXGnmmXpCWi7Dgd0nU_BHG0yFXDJUqXf_8Nm-zK4c' />
+          </div>
+          <div className='content'>
+            <h3>Hello! </h3>
+            <h3>Welcome to my portfolio. My name is Ayarza Manwaring and I recently finished a CS coding bootcamp.</h3>
+          </div>
+          <Link to="/projects"><h1>Check out my projects</h1></Link>
+        </section>
       </div>
     );
   }
