605a91172639b9051e11dd92faed33ee5a3ae82b
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-01 14:55:46 -0400

    added express backend server and hooked it up to the front end to sendemail to me from contact form

---
+++ server/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const creds = require('./config');
+
+var transport = {
+  host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+  port: 587,
+  auth: {
+  user: creds.USER,
+  pass: creds.PASS
+}
+}
+
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+if (error) {
+  console.log(error);
+} else {
+  console.log('Server is ready to take messages');
+}
+});
+
+router.post('/send', (req, res, next) => {
+  var name = req.body.name
+  var email = req.body.email
+  var message = req.body.message
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  var mail = {
+    from: name,
+    to: 'Ayarza.manwaring@gmail.com',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+  
+      transporter.sendMail({
+        from: "<ayarza.manwaring@gmail.com>",
+        to: email,
+        subject: "Submission was successful",
+        text: `Thank you for contacting me! I will get back to you as soon as possible.\n\nForm details\nName: ${name}\n Email: ${email}\n Message: ${message}`
+      }, function(error, info){
+        if(error) {
+          console.log(error);
+        } else{
+          console.log('Message sent: ' + info.response);
+        }
+      });
+    }
+    })
+})
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(3002)
