dcb4a769970d0f445c84971eb354da990209797c
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-09 02:00:24 -0400

    updated CSS, changed Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { ReactComponent as Linkedin } from '../images/svg/logos/linkedin-logo.svg';
 import { ReactComponent as Github } from '../images/svg/logos/github-logo.svg';
+import { Link } from "react-router-dom"
 
 function Footer() {
     return (
         <footer>
             <Navbar bg="light" variant="light">
-                <Navbar.Brand href="/">
+                <Link className="logo-container" to="/">
                     <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
                     <span className='logo-text'>Ayarza<b>Manwaring</b></span>
-                </Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { HashLink as Link } from 'react-router-hash-link';
-import { BrowserRouter as Router, NavLink } from "react-router-dom"
+import { HashLink as HLink } from 'react-router-hash-link';
+import { Link } from "react-router-dom"
 
 function Header() {
     return (
         <header>
-            <Router basename={`${process.env.PUBLIC_URL}/`}> 
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Navbar.Brand href="/">
+                <Link className="logo-container" to="/">
                     <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
                     <span className='logo-text'>Ayarza<b>Manwaring</b></span>
-                </Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link className="m-auto" to="/">Resume</Nav.Link>
-                        <Nav.Link className="m-auto" to="works">Works</Nav.Link>
+                        <Link className="m-auto nav-link" to="/">Resume</Link>
+                        <Link className="m-auto nav-link" to="/works">Works</Link>
                         {/* <Nav.Link className="m-auto" href="/about">About</Nav.Link> */}
-                        <Link className="m-auto nav-link" smooth to="/#about">About</Link>
+                        <HLink className="m-auto nav-link" smooth to="/#about">About</HLink>
 
                         <div className="btn-background">
                             {/* <Nav.Link className="m-auto" href="/contact">Contact</Nav.Link> */}
-                            <Link className="m-auto nav-link" smooth to="/#contact">Contact</Link>
+                            <HLink className="m-auto nav-link" smooth to="/#contact">Contact</HLink>
                         </div>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-        </Router>
         </header>
     );
 }
