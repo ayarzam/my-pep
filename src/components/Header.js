e4a8d32f9aa6e5024f71e426213fefb301cd5e58
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-09 01:16:47 -0400

    attempting to update git hub pages website, navbar is not working but the other links work inclucing the featured click here and the contact form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { HashLink as Link } from 'react-router-hash-link';
+import { BrowserRouter as Router, NavLink } from "react-router-dom"
 
 function Header() {
     return (
         <header>
+            <Router basename={`${process.env.PUBLIC_URL}/`}> 
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Navbar.Brand href="/">
                     <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
@@ -13,8 +15,8 @@ function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link className="m-auto" href="/">Resume</Nav.Link>
-                        <Nav.Link className="m-auto" href="/works">Works</Nav.Link>
+                        <Nav.Link className="m-auto" to="/">Resume</Nav.Link>
+                        <Nav.Link className="m-auto" to="works">Works</Nav.Link>
                         {/* <Nav.Link className="m-auto" href="/about">About</Nav.Link> */}
                         <Link className="m-auto nav-link" smooth to="/#about">About</Link>
 
@@ -25,6 +27,7 @@ function Header() {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+        </Router>
         </header>
     );
 }
