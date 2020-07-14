5d9aeeed588c440a0d5d32f7b1eb986c636e4e10
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-07-13 20:09:46 -0700

    Added tablet/mobile device formatting for the contact section. Also went back and corrected some formatting in the about, hero, header, and footer sections.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,15 +6,15 @@ export default class Contact extends Component {
   render() {
     return (
       <div id="contact-container">
+        <div className="background">
+          <Background />
+        </div>
         <div className="content">
           <div className="contact-text">
             <h2>Contact Me</h2>
             <p>Feel free to reach out and connect!</p>
           </div>
           <ContactForm />
-        </div>
-        <div className="background">
-          <Background />
         </div>
       </div>
     );
