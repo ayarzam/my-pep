115eed6613e99a991b671cdede8f5c735d68b467
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-07-16 01:45:11 -0700

    Added key property to HashLink tags in FullPageNav.js. This causes the className supplied to the HashLink component to update after different nav circles are clicked. Also made a work around that scrolls to the active hashId when there's a reload to redirect from works.

--- src/components/FullPageNav.js
+++ src/components/FullPageNav.js
@@ -44,7 +44,7 @@ export default class FullPageNav extends Component {
             <div id="full-page-nav-container">
                 <ul>
                     <li>
-                        <HashLink className={this.checkPath('/')} to="/" behavior="smooth">
+                        <HashLink key={this.checkPath('/')} className={this.checkPath('/')} to="/" behavior="smooth">
                             <div onClick={() => this.handleClick('/')}>
                                 <span className="full-page-nav-title">Home</span>
                                 <span className="nav-circle"></span>
@@ -52,7 +52,7 @@ export default class FullPageNav extends Component {
                         </HashLink>
                     </li>
                     <li>
-                        <HashLink className={this.checkPath('/#about')} to="/" hashId="#about" behavior="smooth">
+                        <HashLink key={this.checkPath('/#about')} className={this.checkPath('/#about')} to="/" hashId="#about" behavior="smooth">
                             <div onClick={() => this.handleClick('/#about')}>
                                 <span className="full-page-nav-title">About</span>
                                 <span className="nav-circle"></span>
@@ -60,7 +60,7 @@ export default class FullPageNav extends Component {
                         </HashLink>
                     </li>
                     <li>
-                        <HashLink className={this.checkPath('/#featured')} to="/" hashId="#featured" behavior="smooth">
+                        <HashLink key={this.checkPath('/#featured')} className={this.checkPath('/#featured')} to="/" hashId="#featured" behavior="smooth">
                             <div onClick={() => this.handleClick('/#featured')}>
                                 <span className="full-page-nav-title">Featured</span>
                                 <span className="nav-circle"></span>
@@ -68,7 +68,7 @@ export default class FullPageNav extends Component {
                         </HashLink>
                     </li>
                     <li>                   
-                        <HashLink className={this.checkPath('/#contact')} to="/" hashId="#contact" behavior="smooth">
+                        <HashLink key={this.checkPath('/#contact')} className={this.checkPath('/#contact')} to="/" hashId="#contact" behavior="smooth">
                             <div onClick={() => this.handleClick('/#contact')}>
                                 <span className="full-page-nav-title">Contact</span>
                                 <span className="nav-circle"></span>
