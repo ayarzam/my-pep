1e34432a409d675c74ed5362f25141586f43a057
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-07-17 14:24:59 -0700

    Fixed navigation. Moving from works to home correctly scrolls the page and updates the fullPageNav component. Also, clicking on links in the header now correctly update the the fullPageNav components as well.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
         <Header />
 
         <Switch>
-          <Route exact path="/" render={ (routerProps) => <Home {...routerProps} />}></Route>
+          <Route exact path="/" component={Home}></Route>
+          {/* <Route exact path="/" render={ (routerProps) => <Home {...routerProps} />}></Route> */}
           <Route exact path="/works" render={ (routerProps) => <Main {...routerProps} />} ></Route>
           <Route exact path="/works/:id" render={ (routerProps) => <SinglePageView {...routerProps} />} ></Route>
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
   render() {
     return (
       <div id="home-container">
-        <FullPageNav />
+        <FullPageNav key={window.location.hash}/>
         <section id="hero">
           <Hero />
         </section>
