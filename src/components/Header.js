440f10190c8ab8d80ea399ba795cc6e0910cb2d4
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-07-18 01:36:47 -0700

    Made an edit to the nav bar when collapsed so the menu content stands out from the navbar background color. Also, removed the plant that originally blocked the fullPageNav component and fixed the positioning of the arrow-doodle by placing it inside the contact form's grid.

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,11 +21,13 @@ export default class Header extends Component {
         return (
             <header>
                 <Navbar collapseOnSelect expand="lg" bg="light" variant="light" expanded={this.state.expanded}>
-                    <Link className="navbar-brand" to="/">
-                        <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
-                        <span className='logo-text'>Ayarza<b>Manwaring</b></span>
-                    </Link>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => this.setExpanded(this.state.expanded ? false : true)}/>
+                    <div className="header-bar">
+                        <Link className="navbar-brand" to="/">
+                            <img className='logo align-top' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjYdir4W4R7Wy0pwAMxNTfRfxdsCoLViVeueRqeEuwk0zYo1Ox' alt="" />{' '}
+                            <span className='logo-text'>Ayarza<b>Manwaring</b></span>
+                        </Link>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => this.setExpanded(this.state.expanded ? false : true)}/>
+                    </div>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ml-auto">
                             <Link className="m-auto nav-link" to="/" onClick={() => this.setExpanded(false)}>Resume</Link>
