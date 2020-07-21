3f1be4f1f019b91e91a33a692521ea2a905ba852
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-21 15:27:50 -0400

    edited base index file may delete it because it's unnecessary

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const db = require('./server/db/_db');
-const app = require('./server');
-const PORT = 3000;
+// const db = require('./server/db/_db');
+// const app = require('./server');
+// const PORT = 3000;
 
-const init = async () => {
-  await db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
-  console.log('db synced')
-  app.listen(PORT, () => console.log(`server flyin on port ${PORT}`))
-}
+// const init = async () => {
+//   await db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+//   console.log('db synced')
+//   app.listen(PORT, () => console.log(`server flyin on port ${PORT}`))
+// }
 
-init();
+// init();
