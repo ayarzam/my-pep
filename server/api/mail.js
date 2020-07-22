fc9bfc3025cb486e03bbe4d8508c9b54884d83c9
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-21 21:46:35 -0400

    changing mail route to 8080

--- server/api/mail.js
+++ server/api/mail.js
@@ -1,70 +1,70 @@
-const express = require('express');
-const router = express.Router();
-const nodemailer = require('nodemailer');
-const cors = require('cors');
-const creds = require('./config');
+// const express = require('express');
+// const router = express.Router();
+// const nodemailer = require('nodemailer');
+// const cors = require('cors');
+// const creds = require('./config');
 
-const transport = {
-  host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
-  port: 587,
-  auth: {
-  user: creds.USER, 
-  pass: creds.PASS
-}
-}
+// const transport = {
+//   host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+//   port: 587,
+//   auth: {
+//   user: creds.USER, 
+//   pass: creds.PASS
+// }
+// }
 
-var transporter = nodemailer.createTransport(transport)
+// var transporter = nodemailer.createTransport(transport)
 
-transporter.verify((error, success) => {
-if (error) {
-  console.log(error);
-} else {
-  console.log('Server is ready to take messages');
-}
-});
+// transporter.verify((error, success) => {
+// if (error) {
+//   console.log(error);
+// } else {
+//   console.log('Server is ready to take messages');
+// }
+// });
 
-router.post('/send', (req, res, next) => {
-  const name = req.body.name
-  const email = req.body.email
-  const message = req.body.message
-  const content = `name: ${name} \n email: ${email} \n message: ${message} `
+// router.post('/send', (req, res, next) => {
+//   const name = req.body.name
+//   const email = req.body.email
+//   const message = req.body.message
+//   const content = `name: ${name} \n email: ${email} \n message: ${message} `
 
-  const mail = {
-    from: name,
-    to: creds.USER,  // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content
-  }
+//   const mail = {
+//     from: name,
+//     to: creds.USER,  // Change to email address that you want to receive messages on
+//     subject: 'New Message from Contact Form',
+//     text: content
+//   }
 
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
+//   transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       res.json({
+//         status: 'fail'
+//       })
+//     } else {
+//       res.json({
+//        status: 'success'
+//       })
   
-      transporter.sendMail({
-        from: creds.USER,
-        to: email,
-        subject: "Submission was successful",
-        text: `Thank you for contacting me! I will get back to you as soon as possible.\n\nForm details\nName: ${name}\n Email: ${email}\n Message: ${message}`
-      }, function(error, info){
-        if(error) {
-          console.log(error);
-        } else{
-          console.log('Message sent: ' + info.response);
-        }
-      });
-    }
-    })
-})
+//       transporter.sendMail({
+//         from: creds.USER,
+//         to: email,
+//         subject: "Submission was successful",
+//         text: `Thank you for contacting me! I will get back to you as soon as possible.\n\nForm details\nName: ${name}\n Email: ${email}\n Message: ${message}`
+//       }, function(error, info){
+//         if(error) {
+//           console.log(error);
+//         } else{
+//           console.log('Message sent: ' + info.response);
+//         }
+//       });
+//     }
+//     })
+// })
 
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.listen(process.env.PORT || 3002)
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+// app.use('/', router)
+// app.listen(process.env.PORT || 8000)
