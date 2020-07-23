0e9e4d7f401c8ef4430f3e7c8dd3bed9c257568f
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-22 22:08:20 -0400

    still attempting to get nodemailer working

--- server/api/config.js
+++ server/api/config.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv');
-const result = dotenv.config();
-if (result.error) {
-  console.log('Caught an error when reading .env file', result.error);
-}
-// require('dotenv').config()
+// const dotenv = require('dotenv');
+// const result = dotenv.config();
+// if (result.error) {
+//   console.log('Caught an error when reading .env file', result.error);
+// }
+// // require('dotenv').config()
 
-module.exports = {
-  USER: process.env.NM_USERNAME, 
-  PASS: process.env.NM_PASSWORD
-}
+// module.exports = {
+//   USER: process.env.NM_USERNAME, 
+//   PASS: process.env.NM_PASSWORD
+// }
