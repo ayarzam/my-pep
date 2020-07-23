4fc2ade85d13921b01a04f8709d707331d74fbce
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-23 17:24:31 -0400

    attempting to add Database

--- server/db/_db.js
+++ server/db/_db.js
@@ -7,7 +7,7 @@ console.log(chalk.yellow("Opening database connection"));
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  'postgres://qewokqmafwhpsi:83c56c603c58423afc7363a72c0fadc3dec1a0b2c83557bb3ef00ae8e77b64b5@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d6rq70ovn76n2c' || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     ssl: {
       rejectUnauthorized: false
