2fb6dfed9d7c76934e39622af9902dde73a86715
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-23 19:30:44 -0400

    attempting to get postgres logs

--- server/db/_db.js
+++ server/db/_db.js
@@ -9,10 +9,7 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    ssl: {
-      rejectUnauthorized: false
-    },
-    logging: false
+    logging: true
   }
 )
 module.exports = db;
