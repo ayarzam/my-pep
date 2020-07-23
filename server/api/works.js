d1225d2215369129ae677dd0ede98ee5099c92ba
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-23 19:14:46 -0400

    attempting to connect with database

--- server/api/works.js
+++ server/api/works.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const { Works } = require('../db/models/works')
+const Works  = require('../db/models/works')
 const morgan = require('morgan');
+const db = require('../db/_db')
 // const creds = require('./config');
 const nodemailer = require('nodemailer');
 
@@ -14,7 +15,7 @@ router.use(cors());
 
 router.get('/works', async(req, res, next) => {
   try {
-    const allWorks = await Works.findAll(req.params)
+    const allWorks = await db.Works.findAll(req.params)
     res.json(allWorks)
   } catch (error) {
     console.log(error)
@@ -26,7 +27,7 @@ router.get('/works/:id', async (req, res, next) => {
   try {
     console.log('in the try')
     console.log('params', req.params.id)
-    const singleWork = await Works.findByPk(req.params.id)
+    const singleWork = await db.Works.findByPk(req.params.id)
     res.json(singleWork)
   } catch (error) {
     console.log(error)
