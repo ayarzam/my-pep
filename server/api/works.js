62cb0d3568ea9c16f63127584811667730aa9722
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-23 20:19:30 -0400

    fixing works file to run properly

--- server/api/works.js
+++ server/api/works.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const Works  = require('../db/models/works')
+const{ Works } = require('../db/models/works')
 const morgan = require('morgan');
 const db = require('../db/_db')
 // const creds = require('./config');
