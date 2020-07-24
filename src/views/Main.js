e537f7a12dd4d4b0f054bd1784537666f6f59d6f
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-23 21:54:35 -0400

    attmepting to get database to work in heroku

--- src/views/Main.js
+++ src/views/Main.js
@@ -14,7 +14,7 @@ export default class Main extends Component{
     }
   }
   async componentDidMount(){
-    const url = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.REACT_APP_API_URL;
+    const url = process.env.NODE_ENV === 'production' ? 'postgres://qewokqmafwhpsi:83c56c603c58423afc7363a72c0fadc3dec1a0b2c83557bb3ef00ae8e77b64b5@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d6rq70ovn76n2c' : process.env.REACT_APP_API_URL;
     const response = await axios.get(`${url}/api/works`)
     console.log(response)
     const projectData = response.data;
