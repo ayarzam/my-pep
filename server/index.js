3960eb8d5d795aa72f9759b02d3a5a1de6a6e133
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-07-26 21:15:35 -0400

    redirect http to https

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const createApp = () => {
   if (process.env.NODE_ENV === 'production') {
       app.use((req, res, next) => {
         if (req.headers['x-forwarded-proto'] !== 'https')
-          res.redirect('https://' + req.header.host + req.url)
+          res.redirect('https://' + req.headers.host + req.url)
         else
           next()
       })
