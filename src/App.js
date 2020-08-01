ebb356138ffc466e787b4481c390b288a4a0abc0
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-07-31 23:28:01 -0700

    Continuing to clean up the css/scss files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './css/App.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ScrollButton from './components/ScrollBtn';
 import Header from './components/Header';
