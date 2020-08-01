9de0ae85972c4e1efaafbadecd8f16a15cce16b2
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-08-01 01:07:31 -0400

    https

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'https://localhost:8080',
       changeOrigin: true,
     })
   );
