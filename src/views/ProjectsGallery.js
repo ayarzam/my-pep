996eb0d1af23e5b5d892f5050c206bfad08f909d
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-08-01 01:57:33 -0400

    deleted console logs

--- src/views/ProjectsGallery.js
+++ src/views/ProjectsGallery.js
@@ -21,8 +21,6 @@ export default class ProjectsGallery extends Component{
   }
   
   render(){
-    console.log("state: ", this.state)
-    console.log("projects: ", this.state.projects)
     return(
       <div id="projects-gallery-container">
         <CardColumns>
