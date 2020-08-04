701366b6964d45ee786ea27c08a6b0116a750599
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-08-04 00:09:39 -0400

    trying to deploy a production version of our project

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const db = require('./db/_db');
 const socketio = require('socket.io');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const app = express();
 
@@ -62,7 +62,7 @@ const createApp = () => {
 
     // sends index.html if express doesn't recognize the route
     app.use('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client', 'public/index.html'))
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
     });
   }
 
