c0bcc85386580b943a61b058faa3c3c43359cd92
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-08-04 01:55:29 -0400

    changes to ensure that file runs locally and to the mail form in the backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const db = require('./db/_db');
 const socketio = require('socket.io');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const cors = require('cors');
 const app = express();
 
@@ -63,7 +63,7 @@ const createApp = () => {
     // sends index.html if express doesn't recognize the route
     app.use('/*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-    });
+    })
   }
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
